bcc738f876dabea8ed40ec5f5847d7c066011fe9
mingyuShim94 <asdgfhgjk90@gmail.com>
2024-06-24 23:11:29 +0900

    +searchbar_modify

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -2,10 +2,23 @@
 
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { FaCaretDown } from "react-icons/fa";
 
+import { Button } from "@/components/ui/button";
 const Page = () => {
   const router = useRouter();
   const [inputData, setInputData] = useState(""); // 추가: 입력 데이터 상태 관리
+  const [showStatusBar, setShowStatusBar] = useState(true);
+  const [showActivityBar, setShowActivityBar] = useState(false);
+  const [showPanel, setShowPanel] = useState(false);
 
   const onClickSearch = () => {
     const formattedInputData = inputData.replace("#", "-");
@@ -25,14 +38,53 @@ const Page = () => {
       <h2 className="text-[15px] font-semibold mb-8 text-center lg:text-[20px] ">
         당신의 챔피언 취향이 성격을 말해줍니다.
       </h2>
-      <input
-        type="text"
-        placeholder="Summoner name + #KR1"
-        className="p-3 text-gray-800 bg-white border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500 lg:w-[270px] w-[220px] mb-4"
-        value={inputData}
-        onChange={(e) => setInputData(e.target.value)}
-        onKeyDown={onKeyDown}
-      />
+      <div className="flex flex-row items-center bg-white p-3 border border-gray-300 rounded-full mb-5">
+        <div>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button
+                variant="outline"
+                className="p-3 w-[100px] h-[50px] justify-between border-none hover:bg-transparent "
+              >
+                <div>지역</div>
+                <FaCaretDown />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-56">
+              <DropdownMenuLabel>Appearance</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuCheckboxItem
+                checked={showStatusBar}
+                onCheckedChange={setShowStatusBar}
+              >
+                Korea
+              </DropdownMenuCheckboxItem>
+              <DropdownMenuCheckboxItem
+                checked={showActivityBar}
+                onCheckedChange={setShowActivityBar}
+                disabled
+              >
+                North America
+              </DropdownMenuCheckboxItem>
+              <DropdownMenuCheckboxItem
+                checked={showPanel}
+                onCheckedChange={setShowPanel}
+              >
+                Europe West
+              </DropdownMenuCheckboxItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+        <input
+          type="text"
+          placeholder="Summoner name + #KR1"
+          className="p-3 text-gray-800 bg-white border-none rounded-md lg:w-[270px] w-[220px] focus:outline-none"
+          value={inputData}
+          onChange={(e) => setInputData(e.target.value)}
+          onKeyDown={onKeyDown}
+        />
+      </div>
+
       <button
         className="bg-blue-600 text-white p-3 rounded-md w-[220px] hover:bg-blue-700 lg:w-[270px] transition-colors"
         onClick={onClickSearch}
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,29 +1,6 @@
-import axios from "axios";
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
 
-export const sleep = (ms: number) => {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-};
-export const fetchGameData = async (id: string) => {
-  try {
-    const response = await axios.get(`/api/riot?id=${id}`);
-    return response.data;
-  } catch (error) {
-    console.error("API 요청 중 오류 발생:", error);
-    return null;
-  }
-};
-
-export const fetchGeminiData = async (data: string) => {
-  console.log("챔피언 숙련도 데이터받음", data);
-  try {
-    const response = await axios.get(`/api/gemini`, {
-      params: {
-        data: JSON.stringify(data),
-      },
-    });
-    return response.data;
-  } catch (error) {
-    console.error("Gemini API 요청 중 오류 발생:", error);
-    return null;
-  }
-};
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
